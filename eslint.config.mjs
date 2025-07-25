fdf784754f89f9296de92ce7c4af191d2f72ec60
Yash <darbaryash44@gmail.com>
2025-07-25 19:04:41 +0530

    fix: update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,7 +15,8 @@ const eslintConfig = [
     rules: {
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "warn",
-      "no-unused-vars": "off"
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-empty-interface": "off",
     }
   }
 ];
