7b83595bf128e299318a9bd5ff26c04938a8b53c
Yash <darbaryash44@gmail.com>
2025-07-25 19:00:38 +0530

    deployed error resolved

--- components/ui/avatar.tsx
+++ components/ui/avatar.tsx
@@ -1,4 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
 
 import * as React from "react";
 import { cn } from "@/lib/utils";
