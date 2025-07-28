807ffc2144108227fc2e1a1860b02a492db4291d
Yash <darbaryash44@gmail.com>
2025-07-28 20:51:40 +0530

    fix:gemini issue

--- lib/gemini/client.ts
+++ lib/gemini/client.ts
@@ -5,7 +5,8 @@ let geminiClient: GoogleGenerativeAI | null = null;
 export function getGeminiClient() {
   if (!geminiClient) {
     // const apiKey = process.env.GEMINI_API_KEY;
-    const apiKey = "AIzaSyCpWe5QqOrBk4Lvzcb1T1sAd-nuJwwpors";
+    // const apiKey = "AIzaSyCpWe5QqOrBk4Lvzcb1T1sAd-nuJwwpors";
+    const apiKey = "AIzaSyC6cvmtqV4Cw7ATua7FHlHh7_SM-d95304";
     console.log("key0", apiKey);
     if (!apiKey) throw new Error("GEMINI_API_KEY is not set in environment variables");
     geminiClient = new GoogleGenerativeAI(apiKey);
